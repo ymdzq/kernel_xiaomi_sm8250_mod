44ae426fcd8f0ada3ae088eec418455006a4ef5d
Cloud_Yun <1770669041@qq.com>
2025-02-11 01:00:21 +0900

    Import lz4kd and lzo-rle compression algorithm

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -20,11 +20,18 @@
 
 static const char * const backends[] = {
 	"lzo",
+        "lzo-rle",
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
 	"lz4hc",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_LZ4KD)
+	"lz4kd",
+#endif
+#if IS_ENABLED(CONFIG_CRYPTO_DEFLATE)
+	"deflate",
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
 	"842",
