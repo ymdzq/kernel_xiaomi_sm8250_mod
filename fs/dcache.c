9c8a13d91d50ab0933df57b11984c9ccdfe465df
Prslc <prslc113@gmail.com>
2025-07-07 22:05:35 +0800

    refactor: Replace direct susfs_task_state access with helper function
    Signed-off-by: Prslc <prslc113@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2194,7 +2194,7 @@ seqretry:
 			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 				continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 				continue;
 			}
 #endif
@@ -2282,7 +2282,7 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 			continue;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(current->susfs_task_state & TASK_STRUCT_NON_ROOT_USER_APP_PROC)) {
+		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
 			continue;
 		}
 #endif
