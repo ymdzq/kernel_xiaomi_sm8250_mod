34f6fcafa0471c2972747b0fff53e5379f0896cd
ymdzq <1422091+ymdzq@users.noreply.github.com>
2025-10-04 23:03:56 +0800

    修复编译中断问题
    修复代码来自豆包AI

--- techpack/audio/dsp/usf.c
+++ techpack/audio/dsp/usf.c
@@ -1903,7 +1903,9 @@ static void usf_compat_xx_info_type(struct us_xx_info_type32 *us_xx_info32,
 
 static int usf_set_tx_info32(struct usf_type *usf, unsigned long arg)
 {
-	struct us_tx_info_type32 config_tx32;
+	struct us_tx_info_type32 config_tx32 = { 
+	    .us_xx_info = { .client_name = 0 }
+	};
 	struct us_tx_info_type config_tx;
 
 	int rc = copy_from_user(&config_tx32,
@@ -1925,7 +1927,9 @@ static int usf_set_tx_info32(struct usf_type *usf, unsigned long arg)
 
 static int usf_set_rx_info32(struct usf_type *usf, unsigned long arg)
 {
-	struct us_rx_info_type32 config_rx32;
+	struct us_rx_info_type32 config_rx32 = { 
+        .us_xx_info = { .client_name = 0 }
+    };
 	struct us_rx_info_type config_rx;
 
 	int rc = copy_from_user(&config_rx32,
