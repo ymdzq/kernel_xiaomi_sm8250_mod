73bc326f4d9966a7e3aa36ed56d678bd92bec3cc
ymdzq <1422091+ymdzq@users.noreply.github.com>
2025-11-23 12:55:27 +0800

    drivers: 支持Re:Kernel
    Co-authored-by: Sakion-Team @ Github
    Co-authored-by: JackA1ltman <cs2dtzq@163.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -59,6 +59,9 @@
 #include <asm/unistd.h>
 #include <asm/siginfo.h>
 #include <asm/cacheflush.h>
+/* REKERNEL */
+#include <../drivers/rekernel/rekernel.h>
+/* REKERNEL */
 #include "audit.h"	/* audit_signal_info() */
 
 /*
@@ -1288,6 +1291,15 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 	}
 #endif
 
+/* REKERNEL */
+	if (start_rekernel_server() == 0) {
+		if (line_is_frozen(current) && (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)) {
+	 			char binder_kmsg[PACKET_SIZE];
+			snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Signal,signal=%d,killer_pid=%d,killer=%d,dst_pid=%d,dst=%d;", sig, task_tgid_nr(p), task_uid(p).val, task_tgid_nr(current), task_uid(current).val);
+	 			send_netlink_message(binder_kmsg, strlen(binder_kmsg));
+		}
+	}
+/* REKERNEL */
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, type);
 		unlock_task_sighand(p, &flags);
