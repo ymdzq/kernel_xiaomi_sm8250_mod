5ae302b17b0c20606b979ba52f46fb2584ea4a6d
ymdzq <1422091+ymdzq@users.noreply.github.com>
2025-12-21 00:48:16 +0800

    Revert "fs: Bump SuSFS to v2.0.0"
    This reverts commit 47ddc50c296f98c441b83a1f44345bfc7962c50b.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,7 +22,7 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_all_procs;
+bool susfs_hide_sus_mnts_for_all_procs = true; // hide sus mounts for all processes by default
 #endif
 
 static __poll_t mounts_poll(struct file *file, poll_table *wait)
