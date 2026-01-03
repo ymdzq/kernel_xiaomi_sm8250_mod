92068d452b36d59a50581c157455c353e717d70e
stic-server-open <1138705738@qq.com>
2026-01-03 15:58:39 +0800

    drivers: tp_common: create game_mode into touchpanel node

--- drivers/input/touchscreen/tp_common.c
+++ drivers/input/touchscreen/tp_common.c
@@ -19,6 +19,7 @@ TS_ENABLE_FOPS(fod_status)
 TS_ENABLE_FOPS(reversed_keys)
 TS_ENABLE_FOPS(pen_enable)
 TS_ENABLE_FOPS(pen_update)
+TS_ENABLE_FOPS(game_mode)
 
 static int __init tp_common_init(void)
 {
--- include/linux/input/tp_common.h
+++ include/linux/input/tp_common.h
@@ -16,3 +16,4 @@ int tp_common_set_fod_status_ops(struct tp_common_ops *ops);
 int tp_common_set_reversed_keys_ops(struct tp_common_ops *ops);
 int tp_common_set_pen_enable_ops(struct tp_common_ops *ops);
 int tp_common_set_pen_update_ops(struct tp_common_ops *ops);
+int tp_common_set_game_mode_ops(struct tp_common_ops *ops);
